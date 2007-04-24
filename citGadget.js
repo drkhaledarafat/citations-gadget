0cd1aafa080ac599f3fbad1deb12d55326d097a0
thefillm <thefillm@d04f9597-c626-0410-a68d-b7021f955b8a>
2007-04-24 22:25:03 +0000

    some css adjustments for link

--- citGadget.js
+++ citGadget.js
@@ -197,7 +197,7 @@ function totalCites(){
     html += "Citations for '<div class='sTerm'>" + author + "</div>' : <div class='citno'>" + total_citations + "</div><br />";
     html += "Cited Publications: <div class='citno'>" + publications + "</div><br />";
     html += "H-Index: <div class='citno'>" + h_index() + "</div><br />";
-    html += "<a class='link' href='http://scholar.google.com/scholar?as_q=" + gOther + "&as_sauthors=" + gAuthor + "' target='_blank'>publications</a><br />";
+    html += "<a class='link' href='http://scholar.google.com/scholar?as_q=" + gOther + "&as_sauthors=" + gAuthor + "' target='_blank'>view publications&nbsp</a><br />";
     html += "</div>";
     
     // Output html in div.
